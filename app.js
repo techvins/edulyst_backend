5183b12346e878233d1f80ae6ac4bfa497fe2d79
Nitishraj822 <89899009+Nitishraj822@users.noreply.github.com>
2024-10-15 16:44:35 +0530

    destructure

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import connectDB from "./db/connectdb.js";
-import college_web from "./routes/college_routes/web.js";
-import course_web from "./routes/course_routes/web.js";
+import admin_web from "./routes/admin_routes/web.js";
 const app = express()
 const port = process.env.PORT || '3000'
 const DATABASE_URL =  process.env.DATABASE_URL || "mongodb://root:root@mongodb:27017/";
@@ -13,9 +12,7 @@ connectDB(DATABASE_URL);
 app.use(express.json())
 
 // Load Routes
-app.use("/college",college_web)
-app.use("/course",course_web)
-
+app.use("/manage-admin",admin_web)
 
 app.listen(port, () => {
  console.log(`Server listening at http://localhost:${port}`)
--- routes/course_routes/web.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import CourseController from '../../controllers/course_controllers/courseController.js';
-
-router.get('/', CourseController.getAllDoc)
-router.post('/', CourseController.createDoc)
-router.get('/:id', CourseController.getSingleDocById)
-
-export default router
