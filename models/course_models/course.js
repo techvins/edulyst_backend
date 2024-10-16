22e9ec26924f135e056466d0517e7b90fea537e5
Nitishraj822 <89899009+Nitishraj822@users.noreply.github.com>
2024-10-16 12:32:03 +0530

    fixed issue

--- models/course_models/course.js
+++ models/course_models/course.js
@@ -13,6 +13,21 @@ const courseSchema = new mongoose.Schema({
       type: Number,
       required: true
     },
+    description: {
+      type: String
+    },
+    formfields: [{
+      fieldName: {type:String,required: true}, 
+      fieldType: {type: String,required: true},
+      choices: [{
+        name : String,
+        value: String  
+      }],
+      score:{
+        value : String,
+        score : String
+      }
+    }],
     college: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'College',
@@ -20,5 +35,22 @@ const courseSchema = new mongoose.Schema({
     }
   });
 
+const applicationFormSchema = new mongoose.Schema({
+    studentName: {
+      type: String,
+      required: true
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Course',
+      required: true
+    },
+    formfieldsvalue: [{
+      fieldName: String,
+      value: Number
+    }]
+  });
+
 const CourseModel = mongoose.model('Course', courseSchema);
-export default CourseModel
+const ApplicationFormModel = mongoose.model('ApplicationForm',applicationFormSchema)
+export {CourseModel,ApplicationFormModel};
