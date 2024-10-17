c48585e27e4755e89ed9f3ec6fa3939fc1407564
Nitishraj822 <89899009+Nitishraj822@users.noreply.github.com>
2024-10-17 16:11:46 +0530

    remove comment

--- bull/scoreRollEngineQueue.js
+++ bull/scoreRollEngineQueue.js
@@ -1,14 +1,13 @@
 import { createQueue } from './connectbull.js';
 import {CourseModel,ApplicationFormModel} from '../models/course_models/course.js'
 
-// Create the email queue
 const scoreQueue = createQueue('score-queue');
 
-// Task function to process email sending
+
 scoreQueue.process(async (job) => {
   const { objId } = job.data;
   console.log(`Generate score ${objId}...`);
-  await new Promise((resolve) => setTimeout(resolve, 5000)); // Simulate email sending
+  await new Promise((resolve) => setTimeout(resolve, 5000));
   console.log(`Score generated done ${objId}`);
 });
 
