f3c6f930aae55e67946d541d5ba232bc99b41aca
Nitishraj822 <89899009+Nitishraj822@users.noreply.github.com>
2024-10-22 17:44:53 +0530

    schema update

--- models/course_models/course.js
+++ models/course_models/course.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
+const collegeApplicationFormSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: Number,
+    default: 1
+  },
+  college: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'College',
+    required: true
+  },
+  formfields: [{
+    fieldName: {type:String,required: true}, 
+    fieldType: {type: String,required: true},
+    choices: [{
+      name : String,
+      value: String  
+    }],
+    score:{
+      value : String,
+      score : String
+    }
+  }],
+});
+
 const courseSchema = new mongoose.Schema({
     name: {
       type: String,
@@ -20,8 +48,13 @@ const courseSchema = new mongoose.Schema({
       type: mongoose.Schema.Types.ObjectId,
       ref: 'College',
       required: true
-    }
+    },
+    applicationForms: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'CollegeApplicationForm' 
+    }]
   });
 
 const CourseModel = mongoose.model('Course', courseSchema);
-export {CourseModel};
+const CollegeApplicationFormModel = mongoose.model('CollegeApplicationForm',collegeApplicationFormSchema);
+export {CourseModel,CollegeApplicationFormModel};
---
+++ models/student_models/student.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const StudentApplicationSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true
+    },
+    course: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'StudentCourse' 
+    }],
+    college: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'College',
+      required: true
+    },
+    formfields: [{
+        fieldName: {
+          type: String,
+          required: true
+        }, 
+        fieldvalue: {
+          type: mongoose.Schema.Types.Mixed,
+          required: true
+        }
+      }]
+  });
+
+  const StudentApplicationModel = mongoose.model('StudentApplication', StudentApplicationSchema);
+  export {StudentApplicationModel};
